be602112d01ad72481ef8212b681a801a0f8ba3f
Coen Jacobs <coenjacobs@gmail.com>
2012-05-21 18:39:25 +0200

    Added quotes around post title in email message

--- email-author-on-publish.php
+++ email-author-on-publish.php
@@ -18,7 +18,7 @@ function cj_notify_on_pending_to_publish( $post_id ) {
     	$email_data = array(
     		'to'      => $author->user_email,
     		'subject' => sprintf( __( 'Your post on %1$s has been published!', 'email_author_on_publish' ), get_bloginfo('name') ),
-    		'message' => sprintf( __( 'Your post %1$s on %2$s has been published: %3$s', 'email_author_on_publish' ), $post->post_title, get_bloginfo( 'name' ), get_permalink( $post->ID ) ),
+    		'message' => sprintf( __( 'Your post "%1$s" on %2$s has been published: %3$s', 'email_author_on_publish' ), $post->post_title, get_bloginfo( 'name' ), get_permalink( $post->ID ) ),
     	);
 
     	wp_mail( $email_data['to'], $email_data['subject'], $email_data['message'] );
